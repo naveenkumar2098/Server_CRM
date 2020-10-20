a62d8814e735ee1af70eccabe459114bfd9ddff0
Naveen <kumarn.na20@gmail.com>
2020-10-21 04:54:12 +0530

    Basic server setup

---
+++ app.js
@@ -0,0 +1,10 @@
+require('dotenv').config();
+
+const express = require('express');
+const app = express();
+
+const port = process.env.PORT || 8050;
+
+app.listen(port, () => {
+    console.log(`Server is running on ${port}...`);
+})
